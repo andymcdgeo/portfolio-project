4bab8a6505d6a6317abb9b2571f5f922505b2b5a
Andy <andymcdonald1985@gmail.com>
2020-05-02 12:00:13 +0100

    Blog functionality and polish

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,13 @@ class Blog(models.Model):
     publication_date = models.DateTimeField()
     blog_text = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def summary(self):
+        return self.blog_text[0:100]
+
+    def publication_date_short(self):
+        # https://strftime.org 
+        return self.publication_date.strftime('%B %e %Y')
+
+    def __str__(self):
+        return self.title
